841f430d3247fb16ae231a9584a594ae35babe3a
Bruno Araujo <killerasus@gmail.com>
2011-09-28 09:13:21 +0000

    git-svn-id: https://subversion.assembla.com/svn/adaptive_shooter@3 4bcb948d-9056-44c7-a9fa-a90fd10d4afe
    Former-commit-id: 4ce078d7b54c72e50f28ec35b77721f5bca4c934

--- src/AdaptiveShooter/main.cpp
+++ src/AdaptiveShooter/main.cpp
@@ -1,30 +1,55 @@
 #include <ClanLib/core.h>
+#include <ClanLib/display.h>
+#include <ClanLib/gl.h>
 #include <ClanLib/application.h>
 
-class ConsoleProgram
+class DisplayProgram
 {
 public:
-	static int main(const std::vector<CL_String> &args);
+	static int main(const std::vector<CL_String> &args)
+	{
+		CL_SetupCore setup_core;
+		CL_SetupDisplay setup_display;
+		CL_SetupGL setup_gl;
+
+		try
+		{
+			CL_DisplayWindow window("Hello World", 640, 480);
+
+			CL_GraphicContext gc = window.get_gc();
+			CL_InputDevice keyboard = window.get_ic().get_keyboard();
+			CL_Font font(gc, "Tahoma", 30);
+
+			while (!keyboard.get_keycode(CL_KEY_ESCAPE))
+			{
+				gc.clear(CL_Colorf::cadetblue);
+
+				CL_Draw::line(gc, 0, 110, 640, 110, CL_Colorf::yellow);
+				font.draw_text(gc, 100, 100, "Hello World!", CL_Colorf::lightseagreen);
+
+				// Make the stuff visible:
+				window.flip();
+
+				// Read messages from the windowing system message queue, if any are available:
+				CL_KeepAlive::process();
+
+				// Avoid using 100% CPU in the loop:
+				CL_System::sleep(10);
+			}
+		}
+		catch(CL_Exception &exception)
+		{
+			// Create a console window for text-output if not available
+			CL_ConsoleWindow console("Console", 80, 160);
+			CL_Console::write_line("Exception caught: " + exception.get_message_and_stack_trace());
+			console.display_close_message();
+
+			return -1;
+		}
+
+		return 0;
+	}
 };
 
-// Create global application object:
-// You MUST include this line or the application start-up will fail to
-// locate your console application object.
-CL_ClanApplication app(&ConsoleProgram::main);
-
-int ConsoleProgram::main(const std::vector<CL_String> &args)
-{
-	// Setup clanCore:
-	CL_SetupCore setup_core;
-
-	// Create a console Window if one does not exist:
-	CL_ConsoleWindow console_window("Console");
-
-	// Write a line to the console:
-	CL_Console::write_line("Hello World!");
-
-	// Wait for a key so the window won't close immediately.
-	CL_Console::wait_for_key();
-
-	return 0;
-}
+// Create global application object, you MUST include this line or the application start-up will fail to locate your application object.
+CL_ClanApplication app(&DisplayProgram::main);
