8aa2757aad48d7f9ad900e94467aa10f3e41bf58
Bruno Baère Pedrazzi Lomba de Araujo <killerasus@gmail.com>
2026-09-16 20:42:14 -0300

    refactor: update build dependencies
    Updates the build with cmake to recursively download the dependencies.

--- FindClanLib.cmake
+++ FindClanLib.cmake
@@ -3,8 +3,8 @@
 # available from http://clanlib.org.
 #
 # Please define the following before starting this module
-#  ClanLib_MAJOR_VERSION
-#  ClanLib_MINOR_VERSION
+#  ClanLib_MAJOR_VERSION (default 3)
+#  ClanLib_MINOR_VERSION (default 0)
 #
 # The following are defined by this module:
 #  ClanLib_FOUND - TRUE if ClanLib was found
@@ -12,15 +12,20 @@
 #  ClanLib_LIBRARIES - If invoked via FIND_PACKAGE(ClanLib COMPONENTS ...),
 #                      will only contain the libraries matching each component.
 #                      otherwise, it will contain all ClanLib libraries found.
-# For the components Core, App, Display, GL, GUI, GUIStyleSilver, MikMod,
-# Network, SDL, Signals, Sound and Vorbis, the following variables are set:
-#  ClanLib_${COMPONENT}_LIBRARY - Full path to the component's library.
-IF(ClanLib_INCLUDE_DIRS)
-  SET(ClanLib_FIND_QUIETLY TRUE)
-ENDIF(ClanLib_INCLUDE_DIRS)
 
-IF(NOT ClanLib_FIND_COMPONENTS)
-  SET(ClanLib_FIND_COMPONENTS
+if(NOT ClanLib_MAJOR_VERSION)
+  set(ClanLib_MAJOR_VERSION 3)
+endif()
+if(NOT ClanLib_MINOR_VERSION)
+  set(ClanLib_MINOR_VERSION 0)
+endif()
+
+if(ClanLib_INCLUDE_DIRS)
+  set(ClanLib_FIND_QUIETLY TRUE)
+endif(ClanLib_INCLUDE_DIRS)
+
+if(NOT ClanLib_FIND_COMPONENTS)
+  set(ClanLib_FIND_COMPONENTS
     App
     Display
     GL
@@ -32,69 +37,122 @@ IF(NOT ClanLib_FIND_COMPONENTS)
     Signals
     Sound
     Vorbis)
-ENDIF(NOT ClanLib_FIND_COMPONENTS)
+endif(NOT ClanLib_FIND_COMPONENTS)
 
-MACRO(ClanLib_MSG MSG)
-  IF(NOT ClanLib_FIND_QUIETLY)
-    MESSAGE(STATUS ${MSG})
-  ENDIF(NOT ClanLib_FIND_QUIETLY)
-ENDMACRO(ClanLib_MSG)
+macro(ClanLib_MSG MSG)
+  if(NOT ClanLib_FIND_QUIETLY)
+    message(STATUS "${MSG}")
+  endif()
+endmacro(ClanLib_MSG)
 
-MACRO(ClanLib_ERR MSG)
-  IF(ClanLib_FIND_REQUIRED)
-    MESSAGE(SEND_ERROR ${MSG})
-  ELSE(ClanLib_FIND_REQUIRED)
-    ClanLib_MSG(${MSG})
-  ENDIF(ClanLib_FIND_REQUIRED)
-ENDMACRO(ClanLib_ERR)
+# Search paths candidate list
+set(_CLANLIB_SEARCH_PATHS
+  ${CLANLIB_ROOT_DIR}
+  ${ClanLib_ROOT_DIR}
+  ${ClanLib_DIR}
+  ${CMAKE_SOURCE_DIR}/dependencies/clanlib
+  ${CMAKE_SOURCE_DIR}/dependencies/clanlib/install
+  ${CMAKE_SOURCE_DIR}/../dependencies/clanlib
+  ${CMAKE_SOURCE_DIR}/../dependencies/clanlib/install
+  /usr/local
+  /usr
+)
 
-MACRO(ClanLib_FIND_COMPONENT COMPONENT)
+macro(ClanLib_FIND_COMPONENT COMPONENT)
   ClanLib_MSG("Checking for Clan${COMPONENT}")
-  FIND_LIBRARY(ClanLib_${COMPONENT}_LIBRARY clan${ClanLib_MAJOR_VERSION}${ClanLib_MINOR_VERSION}${COMPONENT}
-    ${CLANLIB_ROOT_DIR}/lib /lib /usr/lib /usr/local/lib
-    DOC "Library name for clan${COMPONENT}.")
-  IF(ClanLib_${COMPONENT}_LIBRARY)
-    SET(ClanLib_${COMPONENT}_FOUND TRUE)
+  find_library(ClanLib_${COMPONENT}_LIBRARY
+    NAMES
+      clan${ClanLib_MAJOR_VERSION}${ClanLib_MINOR_VERSION}${COMPONENT}
+      clan${COMPONENT}
+      clan${COMPONENT}-static-mt
+      clan${COMPONENT}-static-mt-debug
+      clan${COMPONENT}-dll
+      clan${COMPONENT}-dll-debug
+      clan${COMPONENT}d
+    PATHS
+      ${_CLANLIB_SEARCH_PATHS}
+    PATH_SUFFIXES
+      lib
+      lib/Win32
+      lib/x64
+      Release
+      Debug
+      x64/Release
+      x64/Debug
+      Win32/Release
+      Win32/Debug
+      install/lib
+      build/lib
+    DOC "Library name for clan${COMPONENT}."
+  )
+  if(ClanLib_${COMPONENT}_LIBRARY)
+    set(ClanLib_${COMPONENT}_FOUND TRUE)
     ClanLib_MSG("Checking for Clan${COMPONENT} -- ${ClanLib_${COMPONENT}_LIBRARY}")
-  ELSE(ClanLib_${COMPONENT}_LIBRARY)
-    SET(ClanLib_${COMPONENT}_FOUND FALSE)
-    IF(ClanLib_FIND_REQUIRED_${COMPONENT})
-      ClanLib_ERR("Checking for Clan${COMPONENT} -- not found")
-    ELSE(ClanLib_FIND_REQUIRED_${COMPONENT})
+  else()
+    set(ClanLib_${COMPONENT}_FOUND FALSE)
+    if(ClanLib_FIND_REQUIRED_${COMPONENT})
+      ClanLib_MSG("Checking for Clan${COMPONENT} -- not found (required)")
+    else()
       ClanLib_MSG("Checking for Clan${COMPONENT} -- not found")
-    ENDIF(ClanLib_FIND_REQUIRED_${COMPONENT})
-  ENDIF(ClanLib_${COMPONENT}_LIBRARY)
-ENDMACRO(ClanLib_FIND_COMPONENT)
+    endif()
+  endif()
+endmacro(ClanLib_FIND_COMPONENT)
 
 ClanLib_MSG("Checking for ClanLib")
-FIND_PATH(ClanLib_INCLUDE_DIRS ClanLib/core.h
-  ${ClanLib_ROOT_DIR}/include ${ClanLib_ROOT_DIR}/include/ClanLib-${ClanLib_MAJOR_VERSION}.${ClanLib_MINOR_VERSION}
-  /usr/local/include          /usr/local/include/ClanLib-${ClanLib_MAJOR_VERSION}.${ClanLib_MINOR_VERSION}
-  /usr/include                /usr/include/ClanLib--${ClanLib_MAJOR_VERSION}.${ClanLib_MINOR_VERSION}
-  DOC "Where to find the ClanLib includes.")
-IF(ClanLib_INCLUDE_DIRS)
-  ClanLib_MSG("Checking for ClanLib -- headers")
-ELSE(ClanLib_INCLUDE_DIRS)
-  ClanLib_ERR("Checking for ClanLib -- headers not found")
-ENDIF(ClanLib_INCLUDE_DIRS)
+find_path(ClanLib_INCLUDE_DIRS ClanLib/core.h
+  PATHS
+    ${_CLANLIB_SEARCH_PATHS}
+  PATH_SUFFIXES
+    include
+    include/ClanLib-${ClanLib_MAJOR_VERSION}.${ClanLib_MINOR_VERSION}
+    Sources/API
+    Sources
+    install/include
+  DOC "Where to find the ClanLib includes."
+)
+
+if(ClanLib_INCLUDE_DIRS)
+  ClanLib_MSG("Checking for ClanLib -- headers found at ${ClanLib_INCLUDE_DIRS}")
+else()
+  ClanLib_MSG("Checking for ClanLib -- headers not found")
+endif()
 
 ClanLib_FIND_COMPONENT(Core)
-IF(ClanLib_INCLUDE_DIRS AND ClanLib_Core_LIBRARY)
-  SET(ClanLib_FOUND TRUE)
-  SET(ClanLib_LIBRARIES ${ClanLib_Core_LIBRARY})
-ELSE(ClanLib_INCLUDE_DIRS AND ClanLib_Core_LIBRARY)
-  SET(ClanLib_FOUND FALSE)
-ENDIF(ClanLib_INCLUDE_DIRS AND ClanLib_Core_LIBRARY)
+if(ClanLib_INCLUDE_DIRS AND ClanLib_Core_LIBRARY)
+  set(ClanLib_FOUND TRUE)
+  set(ClanLib_LIBRARIES ${ClanLib_Core_LIBRARY})
+else()
+  set(ClanLib_FOUND FALSE)
+endif()
 
 ClanLib_MSG("Checking for other ClanLib components")
-FOREACH(COMPONENT ${ClanLib_FIND_COMPONENTS})
+foreach(COMPONENT ${ClanLib_FIND_COMPONENTS})
   ClanLib_FIND_COMPONENT(${COMPONENT})
-  IF(ClanLib_${COMPONENT}_LIBRARY)
-    LIST(APPEND ClanLib_LIBRARIES ${ClanLib_${COMPONENT}_LIBRARY})
-  ENDIF(ClanLib_${COMPONENT}_LIBRARY)
-ENDFOREACH(COMPONENT)
+  if(ClanLib_${COMPONENT}_LIBRARY)
+    list(APPEND ClanLib_LIBRARIES ${ClanLib_${COMPONENT}_LIBRARY})
+  endif()
+endforeach(COMPONENT)
 
-MARK_AS_ADVANCED(
+if(NOT ClanLib_FOUND AND ClanLib_FIND_REQUIRED)
+  message(FATAL_ERROR
+    "\n"
+    "========================================================================\n"
+    "ClanLib ${ClanLib_MAJOR_VERSION}.${ClanLib_MINOR_VERSION} was not found!\n\n"
+    "AdaptiveShooter requires ClanLib ${ClanLib_MAJOR_VERSION}.${ClanLib_MINOR_VERSION}.\n"
+    "Search attempted in:\n"
+    "  - CLANLIB_ROOT_DIR / ClanLib_DIR: ${CLANLIB_ROOT_DIR}\n"
+    "  - Submodule: ${CMAKE_SOURCE_DIR}/dependencies/clanlib\n"
+    "  - System locations (/usr/local, /usr)\n\n"
+    "If using git submodules, make sure it is initialized:\n"
+    "    git submodule update --init --recursive\n\n"
+    "And build ClanLib as documented in BUILDING.md.\n"
+    "You can also specify the ClanLib location with:\n"
+    "    cmake -B build -DCLANLIB_ROOT_DIR=<path-to-clanlib>\n"
+    "========================================================================"
+  )
+endif()
+
+mark_as_advanced(
   ClanLib_INCLUDE_DIRS
   ClanLib_App_LIBRARY
   ClanLib_Core_LIBRARY
